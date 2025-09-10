8a4d4c8b7055ea1a5c2433c2a87ba4e33f64a7a5
Jackelin <jackelinvasquezguzman@gmail.com>
2025-09-10 13:34:37 -0600

    -MENU ESTUDIANTE

--- G3570T_357UD14NT1L.py
+++ G3570T_357UD14NT1L.py
@@ -96,6 +96,14 @@ class Student(User):
                             print("Opcion no valida...")
                 case "2":
                     print(f"{"---"*4}INSCRIPCION A CURSOS{"---"*4}")
+                    if not courses_db:
+                        print("No hay cursos disponibles...")
+                    print("Cursos disponibles")
+                    for course in courses_db.values():
+                        print(f"{course.name} - Docente:{course.teacher_assigned.name}")
+                    couse_name= input("Ingrese nombre de curso a inscribir:")
+                    self.inscription()couse_name
+
                 case "3":
                     print("Salieno del sistema...")
                     break
