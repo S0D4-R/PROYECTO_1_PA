beb6ca48c938d7b6c8a97a54c94727e7ca389799
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-09-11 11:24:08 -0600

    Creada opción adicional en subir_notas (techer)

--- G3570T_357UD14NT1L.py
+++ G3570T_357UD14NT1L.py
@@ -76,7 +76,7 @@ class Teacher(User):
         return self.__id_cat
 
     def subir_notas(self, curso):
-        print("\n\n---------SUBIR NOTAS----------\n1. Actualizar notas de una actividad\n2. Actualizar curso")
+        print("\n\n---------SUBIR NOTAS----------\n1. Actualizar las notas de todas las actividades\n2. Actualizar notas de una actividad\n3. Actualizar curso")
         option = input("Seleccione una opción: ")
         match option: #Crear función en Cursos() para añadir cualquier nueva actividad al repositorio de los alumnos asignados al curso
             case "1":
@@ -93,7 +93,7 @@ class Teacher(User):
                             while True:
                                 try:
                                     punteo = int(input("Ingrese el punteo: "))
-                                    if punteo <0 or punteo > act[0].valor_n:
+                                    if punteo <1 or punteo > act[0].valor_n:
                                         print(f"El punteo debe ser mayor a 0 y menor a {act[0].valor_n}")
                                     else:
                                         act[0].valor_dc = punteo
@@ -104,6 +104,37 @@ class Teacher(User):
                                     print("Error inesperado", e)
 
             case "2":
+                for actividad in curso.asgnaciones:
+                    actividad.mostrar_datos()
+                id_act = input("Ingrese la ID de la actividad: ")
+                if not any(id_act == actividad.self.id for actividad in curso.asignaciones):
+                    print("No se encontro la actividad")
+                else:
+                    for actividad in curso.asignaciones:
+                        if actividad.id == id_act:
+                            for est in curso.roster_alumnos.values():
+                                actividades = est.assigned_c[curso.id_course][1]
+                                for act in actividades:
+                                    act[0].set_status()
+                                    if act[0].id == id_act:
+                                        if act[1]:
+                                            print("El estudiante realizó su entrega")
+                                        else:
+                                            print("El estudiante aún no ha entregado nada")
+
+                                        while True:
+                                            try:
+                                                punteo = int(input("Ingrese el punteo: "))
+                                                if punteo < 0 or punteo > act[0].valor_n:
+                                                    print(f"El punteo debe ser mayor a 0 y menor a {act[0].valor_n}")
+                                                else:
+                                                    act[0].valor_dc = punteo
+                                                    break
+                                            except ValueError:
+                                                print("Solo puede ingresar números enteros")
+                                            except Exception as e:
+                                                print("Error inesperado", e)
+            case "3":
                 pass
             case _:
                 print("Opción inválida")
