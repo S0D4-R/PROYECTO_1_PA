9d65d34677485b054fa353428471103a4c6487b5
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-09-18 00:49:18 -0600

    MODIFICACION EN MÉTODO 'CALCUALR_NOTA' DE LA CLASE CURSO

--- G3570T_357UD14NT1L.py
+++ G3570T_357UD14NT1L.py
@@ -744,15 +744,12 @@ class Curso:
             print("No hay asignaciones asignadas")
 
     def calcular_nota(self, carnet):
-        nota_final = 0
-        nota = 0
+        nota_obtenida = 0
+        nota_posible = 0
         for asignacion in self.asignaciones:
-            if carnet in asignacion.submission:
-                valor_entregado = asignacion.valor_dc or 0
-                nota_final += valor_entregado
-                nota += asignacion.valor_n
-        return nota_final, nota
-
+            nota_obtenida += asignacion.submission.get(carnet, 0)
+            nota_total_posible += asignacion.valor_n
+        return nota_obtenida, nota_posible
 
 class Actividad:
     def __init__(self, act_id, name, valor_neto, valor_de_calificacion, date, h_apertura, h_cierre, type_a):
